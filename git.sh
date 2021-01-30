f5ba7794be89d24505a4cab4a51fb776e228a01a
kleber <kleber107@gmail.com>
2021-01-30 14:24:57 -0300

    initial

---
+++ git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source $(dirname $0)/credentials.sh
+
+commitMessage="Crontab $USER"
+# gitFull = git pull && git add . && git commit -m "$commitMessage" && git push
+
+paths=(
+    ~/Pictures/wallpapers/wallpapers/paisagens
+    ~/.config/Code/User
+    ~/.vscode/extensions
+    ~/.config/Insomnia
+    ~/shellscripts/git
+)
+
+for path in ${paths[@]}; do
+    echo $path
+    cd $path $gitFull && git pull && git add . && git commit -m "$commitMessage" && git push
+done
