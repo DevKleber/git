e1fc9216f379281a60743d0df0efa84318a19b66
kleber <kleber107@gmail.com>
2021-01-30 14:35:11 -0300

    Crontab

--- git.sh
+++ git.sh
@@ -2,7 +2,6 @@
 source $(dirname $0)/credentials.sh
 
 commitMessage="Crontab $USER"
-# gitFull = git pull && git add . && git commit -m "$commitMessage" && git push
 
 paths=(
     ~/Pictures/wallpapers/wallpapers/paisagens
@@ -14,5 +13,5 @@ paths=(
 
 for path in ${paths[@]}; do
     echo $path
-    cd $path $gitFull && git pull && git add . && git commit -m "$path" && git push
+    cd $path $gitFull && git pull && git add . && git commit -m "$commitMessage" && git push
 done
